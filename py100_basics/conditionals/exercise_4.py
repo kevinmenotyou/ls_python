df53f27ee719d72d827b357245d3bb6e75a98a24
Kevin Hutchison <khutchison08@gmail.com>
2024-11-13 16:20:59 +0000

    Add exercise 4

---
+++ py100_basics/conditionals/exercise_4.py
@@ -0,0 +1,12 @@
+# It's a beautiful day! if weather's value is 'sunny'
+# Grab your umbrella. if weather's value is 'rainy'
+# Let's stay inside. if weather's value is anything else
+
+weather = "sunny"
+
+if weather == "sunny":
+    print("It's a beautiful day!")
+elif weather == "rainy":
+    print ("Grab your umbrella.")
+else:
+    print ("Let's stay inside.")
