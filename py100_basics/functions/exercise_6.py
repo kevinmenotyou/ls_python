5e44e18158c92a15788bae99111df6a9b482ca1a
Kevin Hutchison <khutchison08@gmail.com>
2024-11-13 19:07:51 +0000

    Launch school solution

--- py100_basics/functions/exercise_6.py
+++ py100_basics/functions/exercise_6.py
@@ -1,7 +1,7 @@
 def compare_by_length(string1: str, string2: str):
-    if (len(string1) < len(string2)):
+    if len(string1) < len(string2):
         return -1
-    elif (len(string1) > len(string2)):
+    elif len(string1) > len(string2):
         return 1
     else:
         return 0
