a611fe084c20d25069d4d49129213b2ae1fa8c30
Kevin Hutchison <khutchison08@gmail.com>
2024-11-13 10:41:06 +0000

    Add exercise_5.py

---
+++ py100_basics/loops/exercise_5.py
@@ -0,0 +1,9 @@
+lst = [1, 3, 7, 15]
+index = 0
+
+while index < len(lst):
+    #print(f"Index is: {index}")
+    print(f"Value is: {lst[index]}")
+    index += 1
+else:
+    print("Exiting loop...")
