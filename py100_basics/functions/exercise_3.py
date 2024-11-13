3e246ac4e3e2eaca309b0c99f0e154c347e8fa0a
Kevin Hutchison <khutchison08@gmail.com>
2024-11-13 18:28:03 +0000

    Add exercise 3

---
+++ py100_basics/functions/exercise_3.py
@@ -0,0 +1,5 @@
+def cite(author: str, quote: str):
+    print(f"{author} said: \"{quote}\"")
+
+cite('Bruce Eckel', 'Python is executable pseudocode.')
+# Bruce Eckel said: "Python is executable pseudocode."
