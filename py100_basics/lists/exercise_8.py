6010bdb221f4db4ba44ab676bca1d8471136b3ae
Kevin Hutchison <khutchison08@gmail.com>
2024-12-18 11:33:59 +0000

    Fix my unique solution

--- py100_basics/lists/exercise_8.py
+++ py100_basics/lists/exercise_8.py
@@ -1,8 +1,8 @@
 some_value1 = [0, 1, 0, 0, 1]
 some_value2 = 'I leave you my Kingdom, take good care of it.'
 
-print (some_value1 == list)
-print (some_value2 == list)
+print (type(some_value1) == list)
+print (type9some_value2) == list)
 
 # Launch Code Solution
 isinstance(some_value1, list)  # True
