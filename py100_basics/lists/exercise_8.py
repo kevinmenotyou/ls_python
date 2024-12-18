dce1cc3693e4c64028b204ea07e9d6dacaf0e986
Kevin Hutchison <khutchison08@gmail.com>
2024-12-18 11:32:22 +0000

    Add exercise 7 and 8

---
+++ py100_basics/lists/exercise_8.py
@@ -0,0 +1,9 @@
+some_value1 = [0, 1, 0, 0, 1]
+some_value2 = 'I leave you my Kingdom, take good care of it.'
+
+print (some_value1 == list)
+print (some_value2 == list)
+
+# Launch Code Solution
+isinstance(some_value1, list)  # True
+isinstance(some_value2, list)  # False
