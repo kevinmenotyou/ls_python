3d299812e408c54a62bef58ed0a2b3424c5eb498
Kevin Hutchison <khutchison08@gmail.com>
2024-12-26 15:28:04 +0000

    Add calculator refactor

--- py101/lesson2/calculator.py
+++ py101/lesson2/calculator.py
@@ -1,29 +1,53 @@
+def prompt(message):
+    print(f"==> {message}")
+
+def invalid_number(number_str):
+    try:
+        int(number_str)
+    except ValueError:
+        return True
+
+    return False
+
 print('Welcome to Calculator!')
 
 # Ask the user for the first number.
-print("What's the first number?")
+prompt("What's the first number?")
 number1 = input()
 
+while invalid_number(number1):
+    prompt("Hmm... that doesn't look like a valid number.")
+    number1 = input()
+
 # Ask the user for the second number.
-print("What's the second number?")
+prompt("What's the second number?")
 number2 = input()
+
+while invalid_number(number2):
+    prompt("Hmm... that doesn't look like a valid number.")
+    number2 = input()
 
 print(f'{number1} {number2}')
 
 # Ask the user for an operation to perform.
-print('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide')
+prompt("""What operation would you like to perform?
+1) Add 2) Subtract 3) Multiply 4) Divide""")
 operation = input()
 
+while operation not in ["1", "2", "3", "4"]:
+    prompt('You must choose 1, 2, 3, or 4')
+    operation = input()
+
 # Perform the operation on the two numbers.
-if operation == '1':   # '1' represents addition
-    output = int(number1) + int(number2)
-elif operation == '2': # '2' represents subtraction
-    output = int(number1) - int(number2)
-elif operation == '3': # '3' represents multiplication
-    output = int(number1) * int(number2)
-elif operation == '4': # '4' represents division
-    output = int(number1) / int(number2)
+match operation:
+    case '1':   # '1' represents addition
+        output = int(number1) + int(number2)
+    case '2': # '2' represents subtraction
+        output = int(number1) - int(number2)
+    case '3': # '3' represents multiplication
+        output = int(number1) * int(number2)
+    case '4': # '4' represents division
+        output = int(number1) / int(number2)
 
 # Print the result to the terminal.
 print(f"The result is: {output}")
-
