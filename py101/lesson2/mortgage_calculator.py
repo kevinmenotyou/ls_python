d3e6479b82be3d62e081177e0f5ed73b9c4dd16d
Kevin Hutchison <khutchison08@gmail.com>
2025-01-10 08:13:15 +0000

    Add python program

--- py101/lesson2/mortgage_calculator.py
+++ py101/lesson2/mortgage_calculator.py
@@ -1,22 +1,11 @@
 import json
 
-# Open the JSON file for reading
-with open('mortgage_calculator_messages.json', 'r') as file:
-    DATA = json.load(file)
-
-# Inputs
-# - the loan amount
-# - the Annual Percentage Rate (APR)
-# - the loan duration
-# Outputs
-# - monthly payment to 2 decimal places
-# - loan duration in months
-# Examples
-# - Only numbers are input
-# - Non numbers are inputed
-# - 0 is inputted (divided)
+# DEFINITIONS #
 
 MONTHS_IN_YEAR = 12
+
+with open('mortgage_calculator_messages.json', 'r') as file:
+    DATA = json.load(file)
 
 def get_loan_duration_months(loan_in_years):
     return loan_in_years * MONTHS_IN_YEAR
@@ -47,6 +36,8 @@ def get_valid_numeric_input():
     if (is_valid_numeric_input(my_input)):
         return float(my_input)
     return get_valid_numeric_input()
+
+# MAIN #
 
 print(DATA["welcome_message"])
 
