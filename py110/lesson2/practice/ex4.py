540326b9c398988371f3b16df0286f9b3f2cfb1a
kevinmenotyou <khutchison08@gmail.com>
2025-08-05 06:09:02 +0000

    Add lesson2 practice problems -- sorting

---
+++ py110/lesson2/practice/ex4.py
@@ -0,0 +1,23 @@
+books = [
+    {
+        'title': 'One Hundred Years of Solitude',
+        'author': 'Gabriel Garcia Marquez',
+        'published': '1967',
+    },
+    {
+        'title': 'The Book of Kells',
+        'author': 'Multiple Authors',
+        'published': '800',
+    },
+    {
+        'title': 'War and Peace',
+        'author': 'Leo Tolstoy',
+        'published': '1869',
+    },
+]
+
+def sortByDate(my_object):
+    return int(my_object["published"])
+
+my_sorted_dict = sorted(books, key=sortByDate)
+print (my_sorted_dict)
