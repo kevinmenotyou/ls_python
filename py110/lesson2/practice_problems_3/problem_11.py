f8d4ec1de5dc70ff135b12933f1137f3ca486726
kevinmenotyou <khutchison08@gmail.com>
2025-09-17 06:14:29 +0000

    Add problem_11.py

---
+++ py110/lesson2/practice_problems_3/problem_11.py
@@ -0,0 +1,13 @@
+dict1 = {
+    'first':  ['the', 'quick'],
+    'second': ['brown', 'fox'],
+    'third':  ['jumped'],
+    'fourth': ['over', 'the', 'lazy', 'dog'],
+}
+
+vowels = 'aeiou'
+
+list_of_vowels = [letter for value_list in dict1.values() for list_item in value_list for letter in list_item if letter in vowels]
+
+print(list_of_vowels)
+# ['e', 'u', 'i', 'o', 'o', 'u', 'e', 'o', 'e', 'e', 'a', 'o']
