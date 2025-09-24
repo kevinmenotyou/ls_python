aeca8fac9a5c2e112e8cc52230b4acad2b8712ba
kevinmenotyou <khutchison08@gmail.com>
2025-09-24 05:58:56 +0000

    Add tic tac toe basics

---
+++ py110/lesson3/tictactoe.py
@@ -0,0 +1,28 @@
+board = {
+  1: 'X',
+  2: ' ',
+  3: ' ',
+  4: ' ',
+  5: 'O',
+  6: ' ',
+  7: ' ',
+  8: ' ',
+  9: 'X',
+}
+
+def display_board(board):
+    print('')
+    print('     |     |')
+    print(f"  {board[1]}  |  {board[2]}  |  {board[3]}")
+    print('     |     |')
+    print('-----+-----+-----')
+    print('     |     |')
+    print(f"  {board[4]}  |  {board[5]}  |  {board[6]}")
+    print('     |     |')
+    print('-----+-----+-----')
+    print('     |     |')
+    print(f"  {board[7]}  |  {board[8]}  |  {board[9]}")
+    print('     |     |')
+    print('')
+
+display_board(board)
